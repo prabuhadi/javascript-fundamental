7da6cede64ab9277681811fd9f16fd45e8d45a62
prabuhadi <prabuhadiyo@gmail.com>
2023-01-11 14:31:01 +0700

    Understanding variable and it's value

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+//if (js === "amazing") alert("Javascript is FUN!");
 
 console.log(40 + 69 + 33 - 22);
+console.log(33);
+console.log("Matilda");
+
+let firstName = "Prabu";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let number = 3;
+console.log(number);
+// Reserve js new, function, name
+// modify it using (_) or ($)
+let PI = 3.1415;
+console.log(PI);
+
+//using more descriptive var name
+let myFirstJob = "Programming";
+let myCurrentJob = "Coder";
+
+console.log(myCurrentJob);
