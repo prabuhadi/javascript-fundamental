8654ab489114ceaeebccc4a1fd8304c759f01f25
prabuhadi <prabuhadiyo@gmail.com>
2023-01-12 19:46:25 +0700

    If/else BMI

--- challange.js
+++ challange.js
@@ -4,27 +4,49 @@
 let markWeight = 78;
 let markHeight = 1.69;
 const bodyMassIndex1 = markWeight / markHeight ** 2; //mass in kg, height in meter
-console.log(bodyMassIndex1);
+// console.log(bodyMassIndex1);
 
 let johnWeight = 92;
 let johnHeight = 1.95;
 const bodyMassIndex2 = johnWeight / johnHeight ** 2; //mass in kg, height in meter
-console.log(bodyMassIndex2);
+// console.log(bodyMassIndex2);
 
 const markHigherBMI = bodyMassIndex1 >= bodyMassIndex2;
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
 
 // Data 2
 let markWeight2 = 95;
 let markHeight2 = 1.88;
 const bodyMassIndex3 = markWeight2 / (markHeight2 * markHeight2);
-console.log(bodyMassIndex3);
+// console.log(bodyMassIndex3);
 
 let johnWeight2 = 85;
 let johnHeight2 = 1.76;
 const bodyMassIndex4 = johnWeight2 / (johnHeight2 * johnHeight2);
-console.log(bodyMassIndex4);
+// console.log(bodyMassIndex4);
 
-console.log(bodyMassIndex3 >= bodyMassIndex4);
+// console.log(bodyMassIndex3 >= bodyMassIndex4);
 
 // Coding Challange #2
+
+// Data 1
+if (bodyMassIndex1 > bodyMassIndex2) {
+  console.log(
+    `Mark's BMI (${bodyMassIndex1}) is higher than John's (${bodyMassIndex2})!`
+  );
+} else {
+  console.log(
+    `John's BMI(${bodyMassIndex2}) is higher than Mark's (${bodyMassIndex1})!`
+  );
+}
+
+// Data 2
+if (bodyMassIndex3 > bodyMassIndex4) {
+  console.log(
+    `Mark's BMI (${bodyMassIndex3}) is higher than John's (${bodyMassIndex4})!`
+  );
+} else {
+  console.log(
+    `John's BMI (${bodyMassIndex4}) is higher than Mark's (${bodyMassIndex3})!`
+  );
+}
