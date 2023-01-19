45521a8dd378ac3644af9ead86de5043c5fabab3
prabuhadi <prabuhadiyo@gmail.com>
2023-01-19 18:39:50 +0700

    Function Properties

--- part 2/script.js
+++ part 2/script.js
@@ -8,3 +8,27 @@ if (hasDriverLicense) console.log(`I can drive :D`);
 
 // const interface = "Audio";
 // const private = 123;
+
+function logger() {
+  console.log(`My name is Prabu`);
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor(apple, oranges) {
+  const juice = `Juice with ${apple} apples and ${oranges} oranges`;
+  return juice;
+}
+
+// console.log(apple, oranges);
+// fruitProcessor(5, 2);
+// return juice is function in fruitProcessing
+const juice = fruitProcessor(5, 2);
+console.log(juice);
+
+const orangeJuice = fruitProcessor(0, 4);
+console.log(orangeJuice);
+
+const num = Number("33");
+console.log(num);
