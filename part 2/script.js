bc90c5b742659c983a931e851394774d8efa443b
prabuhadi <prabuhadiyo@gmail.com>
2023-01-19 15:54:14 +0700

    Modify folder into 2 parts and adding strict function

---
+++ part 2/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriverLicense) console.log(`I can drive :D`);
+
+// const interface = "Audio";
+// const private = 123;
