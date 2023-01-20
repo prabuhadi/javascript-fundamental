da63f508dab49070226e2445a398a6db473052a4
prabuhadi <prabuhadiyo@gmail.com>
2023-01-20 11:02:07 +0700

    Function population

--- part 2/assignment.js
+++ part 2/assignment.js
@@ -11,3 +11,25 @@ const describeEngland = describeCountry("England", 80, "London");
 console.log(describeFinland);
 console.log(describePortugal);
 console.log(describeEngland);
+
+// Assignment 2
+
+// Function Expression
+const worldPopulation = function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+};
+
+const chinaPopulation = worldPopulation(1441);
+const indonesiaPopulation = worldPopulation(1276);
+const japanPopulation = worldPopulation(784);
+console.log(chinaPopulation);
+console.log(indonesiaPopulation);
+console.log(japanPopulation);
+
+// Fucntion Declaration
+function percentageOfWorld2(population) {
+  return (population / 7900) * 100;
+}
+
+const population2 = percentageOfWorld2(1751);
+console.log(population2);
